e4086cf7948b7a2a6302ba30614b8037c2816a98
aravindfz <aravind.fz@gmail.com>
2017-03-27 23:17:42 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'angular4-first-app',
+    template: `
+        <h1>Angular4: Up and Running</h1>
+    `
+})
+export class AppComponent { }
